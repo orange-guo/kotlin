21e0013f9155ee45cbf146c8bf35d3695e541ef1
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2022-07-15 13:30:19 +0200

    Fix KT-51478
    The issue was that, when various context elements were available
    to fulfill a need for an extension receiver, but none of them were
    applicable to it, the compiler behaved the same way as if there
    was no extension receiver at all.
    
    https://youtrack.jetbrains.com/issue/KT-51478/Inapplicable-receiver-diagnostic-expected-when-there-are-two-context-receiver-candidates

---
+++ compiler/testData/diagnostics/tests/extensions/contextReceivers/twoReceiverCandidatesError.fir.kt
@@ -0,0 +1,16 @@
+// !LANGUAGE: +ContextReceivers
+
+fun String.foo() {}
+
+context(Int, Double)
+fun bar() {
+    <!UNRESOLVED_REFERENCE!>foo<!>() // should be prohibited
+}
+
+fun main() {
+    with(1) {
+        with(2.0) {
+            bar()
+        }
+    }
+}
