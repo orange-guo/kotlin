de0e63023ca9cdb1c8bdec8da41763537b589676
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2022-08-01 17:56:42 +0200

    Update Kotlin Java toolchain code to use compiler options
    ^KT-27301 In Progress

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
@@ -700,8 +700,6 @@ abstract class KotlinCompile @Inject constructor(
             )
         )
 
-        defaultKotlinJavaToolchain.get().updateJvmTarget(this, args)
-
         if (reportingSettings().buildReportMode == BuildReportMode.VERBOSE) {
             args.reportPerf = true
         }
