a93ac3a9fc0ffb7f88f1420d6783038b07bfaf16
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-11-17 13:57:36 +0100

    FIR2IR: fix generating references to base class field

---
+++ compiler/testData/codegen/box/fieldRename/javaProtectedFieldAndKotlinInvisibleProperty.kt
@@ -0,0 +1,30 @@
+// TARGET_BACKEND: JVM_IR
+// IGNORE_BACKEND_K1: JVM_IR
+// Field VS property: case "reference", protected field, invisible property
+
+// FILE: BaseJava.java
+
+package base;
+
+public class BaseJava {
+    protected String a = "OK";
+}
+
+// FILE: Derived.kt
+
+package derived
+
+import base.BaseJava
+
+open class Intermediate : BaseJava() {
+    private val a = "FAIL"
+}
+
+class Derived : Intermediate() {
+    fun foo() = a
+}
+
+fun box(): String {
+    val d = Derived()
+    return d.foo()
+}
