2c70d839d89f61b0b8e2dd27adca040a66175674
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-12-01 15:27:09 +0100

    LLFirLazyDeclarationResolver: additional replace for local declarations

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/LLFirLazyDeclarationResolver.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/LLFirLazyDeclarationResolver.kt
@@ -7,8 +7,13 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir
 
 import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirResolvableModuleSession
 import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirSessionInvalidator
+import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.fir.ThreadSafeMutableState
+import org.jetbrains.kotlin.fir.analysis.checkers.getContainingClassSymbol
+import org.jetbrains.kotlin.fir.declarations.FirMemberDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
+import org.jetbrains.kotlin.fir.declarations.utils.isLocal
+import org.jetbrains.kotlin.fir.declarations.utils.visibility
 import org.jetbrains.kotlin.fir.render
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.FirLazyDeclarationResolver
@@ -33,6 +38,11 @@ internal class LLFirLazyDeclarationResolver(private val sessionInvalidator: LLFi
                 toPhase = toPhase,
                 checkPCE = true,
             )
+            if (fir is FirMemberDeclaration && fir.resolvePhase < toPhase &&
+                (fir.visibility == Visibilities.Local || fir.getContainingClassSymbol(session)?.isLocal == true)
+            ) {
+                fir.replaceResolvePhase(toPhase)
+            }
         } catch (e: Throwable) {
             sessionInvalidator.invalidate(session)
             throw e
