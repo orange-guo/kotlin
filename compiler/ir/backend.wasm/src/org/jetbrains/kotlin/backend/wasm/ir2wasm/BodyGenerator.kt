f4aa84bbdc848f5fa891744b9a81f8f7dd1c3f8a
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2022-12-13 17:59:17 +0100

    [Wasm] Require location for `buildGetGlobal` and fix all usages

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/ir2wasm/BodyGenerator.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/ir2wasm/BodyGenerator.kt
@@ -189,7 +189,7 @@ class BodyGenerator(
                 generateInstanceFieldAccess(field)
             }
         } else {
-            body.buildGetGlobal(context.referenceGlobalField(field.symbol))
+            body.buildGetGlobal(context.referenceGlobalField(field.symbol), expression.getSourceLocation())
         }
     }
 
@@ -291,9 +291,9 @@ class BodyGenerator(
 
     private fun generateAnyParameters(klassSymbol: IrClassSymbol, location: SourceLocation) {
         //ClassITable and VTable load
-        body.buildGetGlobal(context.referenceGlobalVTable(klassSymbol))
+        body.buildGetGlobal(context.referenceGlobalVTable(klassSymbol), location)
         if (klassSymbol.owner.hasInterfaceSuperClass()) {
-            body.buildGetGlobal(context.referenceGlobalClassITable(klassSymbol))
+            body.buildGetGlobal(context.referenceGlobalClassITable(klassSymbol), location)
         } else {
             body.buildRefNull(WasmHeapType.Simple.Data)
         }
